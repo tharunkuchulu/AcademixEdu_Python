85515a90bd065dbe18018c1649f9622db4557f7d
Tharun Vankayala <tharunvankayala@gmail.com>
2024-11-18 20:59:51 +0530

    clock

--- clock.py
+++ clock.py
@@ -1,4 +1,13 @@
-for H in range(24):
-    for M in range(60):
-        for S in range(60):
-            print(f"{H:02}:{M:02}:{S:02}")
+n=int(input("Enter the number of hours (12 or 24): "))
+if n==12:
+    for H in range(12):
+        for M in range(60):
+            for S in range(60):
+                print(f"{H:02}:{M:02}:{S:02}")
+elif n==24:
+    for H in range(24):
+        for M in range(60):
+            for S in range(60):
+                print(f"{H:02}:{M:02}:{S:02}")
+else:
+    print("Invalid input")
