7e8dfa9a5ba99c8204ea1c96fc6f599b44feb376
Tharun Vankayala <tharunvankayala@gmail.com>
2025-01-22 12:44:06 +0530

    update

--- basics.py
+++ basics.py
@@ -1,2 +1,11 @@
-arr= [1,2,3,4,"jhvb"]
-print(arr)
+name=str(input("Enter your name:"))
+pin=int(input("Enter your pin:"))
+initial_balance=float(input("Enter your initial balance:"))
+
+desposit=float(input("Enter the amount you want to deposit:"))
+initial_balance+=desposit
+print("Your balance is:",initial_balance)
+
+withdraw=float(input("Enter the amount you want to withdraw:"))
+initial_balance-=withdraw
+print("Your balance is:",initial_balance)
